bd0c42902640278813aecfc1125d561f466cab4d
ramin <rx.azhdari@gmail.com>
2023-11-14 17:02:07 +0100

    enh: create command line interface for backtester

--- testing_engines/vectorized_backtester.py
+++ testing_engines/vectorized_backtester.py
@@ -1,7 +1,9 @@
+import os
 import math
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import subprocess
+import argparse
 
 
 def run(
@@ -30,6 +32,7 @@ def run(
 def analyze(
     strategy_name,
     portfolio_weights,
+    # TODO: Make sure portfolio_weights dates and stock_retuns dates are aligned
     stock_returns,
 ):
     return Report(strategy_name, portfolio_weights, stock_returns)
@@ -102,3 +105,87 @@ class Report:
             columns=["Metric"],
             index=["Portfolio return", "Portfolio volatility", "Sharpe ratio"],
         ).round(2)
+
+
+def install_dependency(package_path):
+    subprocess.call(["pip", "install", "-r", package_path + "/requirements.txt"])
+
+
+def main(
+    strategy_path,
+    strategy_name,
+    dataset_path,
+    market_data_path,
+    start_date,
+    window_size,
+):
+    dataset_df = pd.read_csv(dataset_path).set_index("Date")
+
+    install_dependency(strategy_path)
+    strategy = __import__(strategy_name)
+
+    weights = run(strategy, dataset_df, start_date, window_size)
+
+    market_data_df = pd.read_csv(market_data_path).set_index("Date")
+
+    report = analyze(strategy.name, weights, market_data_df.loc[start_date:])
+
+    print("\n----------- RESULTS ----------")
+    print("\n----------- METRICS ----------")
+    print(report.portfolio_metrics())
+    print("\n----------- WEIGHTS ----------")
+    print(report.portfolio_weights)
+    print("\n----------- RETURNS ----------")
+    print(report.portfolio_returns)
+    print("\n----------- END --------------")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="Vectorized Backtester",
+        description="A vectorized backtester to test both quant and ML investment strategies",
+    )
+    parser.add_argument(
+        "-sp", "--strategy-path", help="strategy to be executed", required=True
+    )
+    parser.add_argument(
+        "-sn",
+        "--strategy-name",
+        help="name of the strategy used for import",
+        required=True,
+    )
+    parser.add_argument(
+        "-d", "--dataset", help="dataset to execute the strategy on", required=True
+    )
+    parser.add_argument(
+        "-md",
+        "--market-data",
+        help="dataset for the market data the strategy should trade on",
+        # TODO: This could default to reuse the test data
+        required=True,
+    )
+    parser.add_argument(
+        "-sd",
+        "--start-date",
+        help="the start date to execute the strategy on (in ISO format)",
+        # TODO: This could default to the first day of the dataset
+        required=True,
+    )
+    parser.add_argument(
+        "-ws",
+        "--window-size",
+        help="how many rows from the dataset should be passed to the strategy",
+        const=0,
+        nargs="?",
+        type=int,
+    )
+    args = parser.parse_args()
+
+    main(
+        args.strategy_path.strip(),
+        args.strategy_name.strip(),
+        args.dataset.strip(),
+        args.market_data.strip(),
+        args.start_date.strip(),
+        args.window_size,
+    )
